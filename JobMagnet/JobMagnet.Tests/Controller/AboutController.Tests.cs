6c6d2d58c49b8632e73a8368b761ea03d9685552
Alexandra Marval <alexndra.04@hotmail.com>
2024-09-26 17:24:54 +0200

    ✨Add tests ItShouldShowOk

--- JobMagnet/JobMagnet.Tests/Controller/AboutController.Tests.cs
+++ JobMagnet/JobMagnet.Tests/Controller/AboutController.Tests.cs
@@ -1,11 +1,25 @@
+using JobMagnet.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
 namespace JobMagnet.Tests.Controller
 {
     public class AboutControllerTests
     {
         [Fact]
-        public void Test1()
+        public void ItShouldShowOk()
         {
+            //Arranger Preparar
+            var controller = new AboutController();
 
+            //Act Ejecutar
+            var respuesta = controller.GetOk();
+
+            var okResult = respuesta as OkObjectResult;
+            //Assert Asegurar
+
+            Assert.NotNull(okResult); 
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.NotNull(okResult.Value);
         }
     }
 }
--- JobMagnet/JobMagnet/Controllers/AboutController.cs
+++ JobMagnet/JobMagnet/Controllers/AboutController.cs
@@ -1,7 +1,19 @@
-﻿namespace JobMagnet.Controllers
-{
-    public class AboutController
-    {
+﻿using JobMagnet.Entities;
+using Microsoft.AspNetCore.Mvc;
 
+namespace JobMagnet.Controllers
+{
+    [ApiController]
+    [Route("api/controller")]
+    public class AboutController : ControllerBase
+    {
+        public AboutController() { }
+
+        [HttpGet]
+        public ActionResult GetOk()
+        {
+            var aboutEntity = new AboutEntity();
+            return Ok(aboutEntity);
+        }
     }
 }
