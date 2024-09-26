3247bddc11b954c1d577bce17640e56c1229f905
Alexandra Marval <alexndra.04@hotmail.com>
2024-09-26 11:01:35 +0200

    Create AboutEntity and its properties

---
+++ JobMagnet/JobMagnet/Entities/AboutEntity.cs
@@ -0,0 +1,21 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace JobMagnet.Entities
+{
+    public class AboutEntity
+    {
+        public string Description { get; set; } 
+        public string ImageUrl { get; set; }
+        public string Text { get; set; }
+        public string Hobbies { get; set; }
+        public string Birthday { get; set; }
+        public string WebSite { get; set; }
+        public int PhoneNumber { get; set; }
+        public string City { get; set; }
+        public int Age { get; set; }
+        public string Degree { get; set; }
+        public string Email { get; set; }
+        public string Freelance { get; set; }
+        public string WorkExperience { get; set; }
+    }
+}
