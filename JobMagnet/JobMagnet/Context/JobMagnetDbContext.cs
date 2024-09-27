88ef108f8a17330152e02840b54b4459ac2af2ac
Alexandra Marval <alexndra.04@hotmail.com>
2024-09-27 11:54:18 +0200

    Create a table in the database

--- JobMagnet/JobMagnet/Context/JobMagnetDbContext.cs
+++ JobMagnet/JobMagnet/Context/JobMagnetDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using JobMagnet.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace JobMagnet.Context
 {
     public class JobMagnetDbContext : DbContext
     {
+        public DbSet<AboutEntity> About { get; set; }
         public JobMagnetDbContext(DbContextOptions options) : base (options)
         {
         }
