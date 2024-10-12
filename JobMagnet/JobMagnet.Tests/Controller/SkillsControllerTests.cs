4a839b304b096936a6d9ddcb09610ca6f46a9344
Alexandra Marval <alexndra.04@hotmail.com>
2024-10-12 20:16:18 +0200

    ✨Make sure it returns an ok

---
+++ JobMagnet/JobMagnet.Tests/Controller/SkillsControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using JobMagnet.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+namespace JobMagnet.Tests.Controller
+{
+    public class SkillsControllerTests
+    {
+        public SkillsControllerTests()
+        {
+        }
+
+        [Fact]
+        public void MustReturnOk()
+        {
+            var controller = new SkillsController();
+
+            //Act Ejecutar
+            var respuesta = controller.GetOk();
+
+            //Assert Asegurar
+            var resultEsperado = respuesta as OkResult;
+            resultEsperado.Should().NotBeNull();
+            resultEsperado!.StatusCode.Should().Be((int)HttpStatusCode.OK);
+            resultEsperado.Should().BeEquivalentTo(respuesta);
+        }
+    }
+}
+
---
+++ JobMagnet/JobMagnet/Controllers/SkillsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobMagnet.Controllers
+{
+    [ApiController]
+    [Route("api/controller")]
+    public class SkillsController : ControllerBase
+    {
+        public SkillsController()
+        {          
+        }
+
+        [HttpGet]
+        public IActionResult GetOk() 
+        {
+            return Ok();
+        }
+    }
+}
