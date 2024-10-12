5e89e301891e094102dc230c50b1e2c716a1fbab
Alexandra Marval <alexndra.04@hotmail.com>
2024-10-12 22:46:01 +0200

    ✨Agrega SkillCreateRequest model

---
+++ JobMagnet/JobMagnet/Models/SkillCreateRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace JobMagnet.Models
+{
+    public class SkillCreateRequest
+    {
+        public string Overview { get; set; }
+        public string Rank { get; set; }
+        public string name { get; set; }
+        public string iconUrl { get; set; }
+    }
+}
