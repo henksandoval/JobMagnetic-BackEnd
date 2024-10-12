d8856c3a81d6460d57d0869e9f8ef2e6adadfac6
Alexandra Marval <alexndra.04@hotmail.com>
2024-10-12 22:49:34 +0200

    ✨Add SkillRepository class

---
+++ JobMagnet/JobMagnet/Repository/SkillRepository.cs
@@ -0,0 +1,24 @@
+﻿using JobMagnet.Context;
+using JobMagnet.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobMagnet.Repository
+{
+    public class SkillRepository<TEntity> : ISkillRepository<TEntity> where TEntity : class
+    {
+        private readonly JobMagnetDbContext dbContext;
+        private DbSet<TEntity> dbSet;
+
+        public SkillRepository(JobMagnetDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.dbSet = dbContext.Set<TEntity>();
+        }
+
+        public async Task CreateAsync(TEntity entity)
+        {
+            await dbSet.AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
