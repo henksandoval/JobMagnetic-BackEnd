36323a3a114fd95be1d32fa116a5dba2b376d5fe
Alexandra Marval <alexndra.04@hotmail.com>
2024-10-12 22:48:25 +0200

    ✨Add SkillService class

---
+++ JobMagnet/JobMagnet/Service/SkillService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using JobMagnet.Entities;
+using JobMagnet.Models;
+using JobMagnet.Repository.Interface;
+using JobMagnet.Service.Interface;
+
+namespace JobMagnet.Service
+{
+    public class SkillService : ISkillService
+    {
+        private readonly IMapper mapper;
+        private readonly ISkillRepository<SkillEntity> repository;
+
+        public SkillService(IMapper mapper, ISkillRepository<SkillEntity> repository)
+        {
+            this.mapper = mapper;
+            this.repository = repository;
+        }
+
+        public async Task<SkillModel> Create(SkillCreateRequest skillCreateRequest)
+        {
+            var skillEntity = mapper.Map<SkillEntity>(skillCreateRequest);
+            await repository.CreateAsync(skillEntity);
+
+            var skillModel = mapper.Map<SkillModel>(skillEntity);
+            return skillModel;
+        }
+    }
+}
