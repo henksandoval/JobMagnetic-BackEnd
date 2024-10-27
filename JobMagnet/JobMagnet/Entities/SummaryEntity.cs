b67299353820c54d29f5c870e51476b9f161ec4d
Alexandra Marval <alexndra.04@hotmail.com>
2024-10-27 16:34:09 +0100

    ✨Create summary entity

---
+++ JobMagnet/JobMagnet/Entities/SummaryEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace JobMagnet.Entities
+{
+    public class SummaryEntity
+    {
+        public int Id { get; set; }
+        public string About { get; set; }
+        public string Name { get; set; }
+        public string Introduction { get; set; }
+    }
+}
