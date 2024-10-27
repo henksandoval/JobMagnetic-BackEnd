23366f1ee1fc5890ac9c7e17667bf72a2c34fe1c
Alexandra Marval <alexndra.04@hotmail.com>
2024-10-27 15:31:43 +0100

    ✅Make sure to return an Ok

---
+++ JobMagnet/JobMagnet.Tests/Controller/SummaryControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using JobMagnet.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace JobMagnet.Tests.Controller
+{
+    public class SummaryControllerTests
+    {
+        public SummaryControllerTests()
+        {
+        }
+
+        [Fact]
+        public void ShouldReturnAnOk()
+        {
+            //Arranger Preparar
+            var controller = new SummaryController();
+
+            //Act Ejecutar
+            var response = controller.GetOk();
+
+            //Assert Asegurar
+
+            var expectedResult = response as OkResult;
+            expectedResult.Should().BeEquivalentTo(response);
+            expectedResult!.StatusCode.Should().Be((int)HttpStatusCode.OK);
+            expectedResult.Should().BeEquivalentTo(response);
+        }
+    }
+}
