e26722f4852e6755de9e1e51b4b646afc429a435
Alexandra Marval <alexndra.04@hotmail.com>
2024-10-27 15:30:16 +0100

    ✨Summary controller is created

---
+++ JobMagnet/JobMagnet/Controllers/SummaryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobMagnet.Controllers
+{
+    [ApiController]
+
+    [Route("api/controller")]
+    public class SummaryController : ControllerBase
+    {
+        public SummaryController() { }
+
+        public IActionResult GetOk()
+        {
+            return Ok();
+        }
+    }
+}
