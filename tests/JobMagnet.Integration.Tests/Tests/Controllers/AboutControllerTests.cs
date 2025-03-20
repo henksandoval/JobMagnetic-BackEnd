f6b187897f9b75f56a4aae4dbf4e91c190f7f19d
hsandoval <henk.sandoval@hotmail.com>
2025-03-20 16:38:42 +0100

    test: 👷 Enhance AboutControllerTests with additional async test for record retrieval

--- tests/JobMagnet.Integration.Tests/Tests/Controllers/AboutControllerTests.cs
+++ tests/JobMagnet.Integration.Tests/Tests/Controllers/AboutControllerTests.cs
@@ -17,25 +17,26 @@ public class AboutControllerTests : IClassFixture<JobMagnetTestSetupFixture>
 {
     private const string RequestUriController = "api/about";
     private readonly JobMagnetTestSetupFixture _jobMagnetTestSetupFixture;
+    private readonly HttpClient _httpClient;
+    private readonly Fixture _fixture = new();
 
     public AboutControllerTests(JobMagnetTestSetupFixture jobMagnetTestSetupFixture)
     {
         _jobMagnetTestSetupFixture = jobMagnetTestSetupFixture;
+        _httpClient = _jobMagnetTestSetupFixture.GetClient();
     }
 
     [Fact(DisplayName = "Should create a new About and return 201 when the request is valid")]
-    public async Task ShouldReturnCreatedAndPersistData_WhenRequestIsValid()
+    public async Task ShouldReturnCreatedAndPersistData_WhenRequestIsValidAsync()
     {
         // Arrange
         await using var scope = _jobMagnetTestSetupFixture.GetProvider().CreateAsyncScope();
-        var fixture = new Fixture();
 
         // Act
-        var createRequest = fixture.Build<AboutCreateRequest>().Create();
+        var createRequest = _fixture.Build<AboutCreateRequest>().Create();
         var httpContent = new StringContent(JsonConvert.SerializeObject(createRequest), Encoding.UTF8,
             MediaTypeNames.Application.Json);
-        var httpClient = _jobMagnetTestSetupFixture.GetClient();
-        var response = await httpClient.PostAsync(RequestUriController, httpContent);
+        var response = await _httpClient.PostAsync(RequestUriController, httpContent);
 
         // Assert
         var aboutRepository = scope.ServiceProvider.GetRequiredService<IAboutRepository<AboutEntity>>();
@@ -52,5 +53,27 @@ public class AboutControllerTests : IClassFixture<JobMagnetTestSetupFixture>
         var aboutCreated = await aboutRepository.GetByIdAsync(responseData!.Id);
         aboutCreated.ShouldNotBeNull();
         aboutCreated.Should().BeEquivalentTo(createRequest, options => options.ExcludingMissingMembers());
+    }
+
+    [Fact(DisplayName = "Should return the record when a valid ID is provided")]
+    public async Task ShouldReturnAboutRecord_WhenValidIdIsProvidedAsync()
+    {
+        // Arrange
+        var fixture = new Fixture();
+        await using var scope = _jobMagnetTestSetupFixture.GetProvider().CreateAsyncScope();
+
+        var aboutRepository = scope.ServiceProvider.GetRequiredService<IAboutRepository<AboutEntity>>();
+        var aboutEntity = fixture.Build<AboutEntity>().With(x => x.Id, 0).Create();
+        await aboutRepository.CreateAsync(aboutEntity);
+
+        // Act
+        var httpClient = _jobMagnetTestSetupFixture.GetClient();
+        var response = await httpClient.GetAsync($"{RequestUriController}/{aboutEntity.Id}");
+
+        // Assert
+        var responseData = JsonConvert.DeserializeObject<AboutModel>(await response.Content.ReadAsStringAsync());
+        response.IsSuccessStatusCode.ShouldBeTrue();
+        responseData.ShouldNotBeNull();
+        responseData.Should().BeEquivalentTo(aboutEntity, options => options.ExcludingMissingMembers());
     }
 }
