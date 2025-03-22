18274dc844d253b9c5ad666611b512fd087b87b8
hsandoval <henk.sandoval@hotmail.com>
2025-03-22 10:38:10 +0100

    refactor: 🔧 Introduce AboutMapper and SkillMapper for mapping entities and models using Mapster

---
+++ sources/JobMagnet/Mappers/SkillMapper.cs
@@ -0,0 +1,18 @@
+﻿using JobMagnet.Entities;
+using JobMagnet.Models;
+using Mapster;
+
+namespace JobMagnet.Mappers;
+
+public static class SkillMapper
+{
+    public static SkillModel ToModel(SkillEntity entity)
+    {
+        return entity.Adapt<SkillModel>();
+    }
+
+    public static SkillEntity ToEntity(SkillCreateRequest request)
+    {
+        return request.Adapt<SkillEntity>();
+    }
+}
