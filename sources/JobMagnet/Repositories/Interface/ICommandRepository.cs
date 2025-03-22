4afb313f7d55ec728fed0175a4b0c1e351c92c33
hsandoval <henk.sandoval@hotmail.com>
2025-03-22 09:00:47 +0100

    feat: ✨ Implement ICommandRepository and IQueryRepository interfaces with basic CRUD operations

---
+++ sources/JobMagnet/Repositories/Interface/ICommandRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace JobMagnet.Repositories.Interface;
+
+public interface ICommandRepository<TEntity> where TEntity : class
+{
+    Task<TEntity?> GetByIdAsync(int id);
+    Task CreateAsync(TEntity entity);
+    Task<bool> UpdateAsync(TEntity entity);
+    Task<IDbContextTransaction> BeginTransactionAsync();
+    Task CommitAsync(IDbContextTransaction transaction);
+}
---
+++ sources/JobMagnet/Repositories/Interface/IQueryRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace JobMagnet.Repositories.Interface;
+
+public interface IQueryRepository<TEntity> where TEntity : class
+{
+    Task<TEntity?> GetByIdAsync(int id);
+}
