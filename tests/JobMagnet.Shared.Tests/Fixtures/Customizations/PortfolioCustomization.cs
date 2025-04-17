3de3ec7f027b709d8a765906b6a07d3701cd447b
Henk Sandoval <henk.sandoval@hotmail.com>
2025-04-18 01:04:06 +0200

    refactor: ♻️ Update Portfolio and Resume customizations to include ProfileId assignment for improved consistency

--- tests/JobMagnet.Shared.Tests/Fixtures/Customizations/PortfolioCustomization.cs
+++ tests/JobMagnet.Shared.Tests/Fixtures/Customizations/PortfolioCustomization.cs
@@ -13,6 +13,7 @@ public class PortfolioCustomization : ICustomization
                 .With(x => x.IsDeleted, false)
                 .Without(x => x.DeletedAt)
                 .Without(x => x.DeletedBy)
+                .With(x => x.ProfileId, 0)
                 .With(x => x.Profile, fixture.Create<ProfileEntity>())
                 .With(x => x.GalleryItems, fixture.CreateMany<PortfolioGalleryItemEntity>().ToList())
                 .OmitAutoProperties()
