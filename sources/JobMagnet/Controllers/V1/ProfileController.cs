117ebd80052b1c9ce94b57a039883f6ef35595c8
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-04-18 20:54:41 +0200

    fix: 🐛 Remove unnecessary newline in ProfileController response model method

--- sources/JobMagnet/Controllers/V1/ProfileController.cs
+++ sources/JobMagnet/Controllers/V1/ProfileController.cs
@@ -28,7 +28,6 @@ public class ProfileController(
             return Results.NotFound();
 
         var responseModel = ProfileMapper.ToModel(entity);
-
         return Results.Ok(responseModel);
     }
 }
