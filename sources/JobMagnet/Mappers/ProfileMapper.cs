5304321ea2c12dd622f20ac3e2f30c1815fad85f
Henk Sandoval <henk.sandoval@hotmail.com>
2025-04-18 13:23:05 +0200

    test: ✅ Add test by assert that TestimonialMapper work right

--- sources/JobMagnet/Mappers/ProfileMapper.cs
+++ sources/JobMagnet/Mappers/ProfileMapper.cs
@@ -3,6 +3,7 @@ using JobMagnet.Infrastructure.Entities;
 using JobMagnet.ViewModels.Profile;
 using Mapster;
 
+// ReSharper disable ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
 namespace JobMagnet.Mappers;
 
 internal static class ProfileMapper
@@ -11,31 +12,45 @@ internal static class ProfileMapper
     {
         TypeAdapterConfig<ProfileEntity, ProfileViewModel>
             .NewConfig()
-            .Map(dest => dest.PersonalData, src => new PersonalDataViewModel(
-                GetFullName(src),
-                src.Talents.Select(t => t.Description).ToArray(),
-                src.Resume.ContactInfo.Select(c => new SocialNetworksViewModel(
-                        c.ContactType.Name,
-                        c.Value,
-                        c.ContactType.IconClass,
-                        c.ContactType.IconUrl))
-                    .ToArray()
-            ))
+            .Map(dest => dest.PersonalData,
+                src => new PersonalDataViewModel(
+                    GetFullName(src),
+                    src.Talents.Select(t => t.Description).ToArray(),
+                    src.Resume.ContactInfo.Select(c => new SocialNetworksViewModel(
+                            c.ContactType.Name,
+                            c.Value,
+                            c.ContactType.IconClass ?? string.Empty,
+                            c.ContactType.IconUrl ?? string.Empty))
+                        .ToArray()
+                ),
+                src => src.Resume != null && src.Resume.ContactInfo.Any() && src.Talents.Any()
+            )
             .Map(dest => dest.About, src => new AboutViewModel(
-                src.ProfileImageUrl,
-                src.Resume.About,
-                src.Resume.JobTitle,
-                src.Resume.Overview,
-                src.BirthDate!.Value,
-                GetContactValue(src, "Website"),
-                GetContactValue(src, "Mobile Phone"),
-                src.Resume.Address!,
-                src.BirthDate.GetAge(),
-                src.Resume.Title!,
-                GetContactValue(src, "Email"),
-                "",
-                src.Resume.Summary
-            ));
+                    src.ProfileImageUrl,
+                    src.Resume.About,
+                    src.Resume.JobTitle,
+                    src.Resume.Overview,
+                    src.BirthDate!.Value,
+                    GetContactValue(src, "Website"),
+                    GetContactValue(src, "Mobile Phone"),
+                    src.Resume.Address ?? string.Empty,
+                    src.BirthDate.GetAge(),
+                    src.Resume.Title ?? string.Empty,
+                    GetContactValue(src, "Email"),
+                    "",
+                    src.Resume.Summary
+                ),
+                src => src.Resume != null && src.BirthDate != null
+            )
+            .Map(dest => dest.Testimonials, src => src.Testimonials
+                    .Select(t => new TestimonialsViewModel(
+                        t.Name,
+                        t.JobTitle,
+                        t.PhotoUrl,
+                        t.Feedback))
+                    .ToArray(),
+                src => src.Testimonials.Any()
+            );
     }
 
     internal static ProfileViewModel ToModel(ProfileEntity entity)
@@ -47,8 +62,10 @@ internal static class ProfileMapper
         string.Join(" ", new[] { entity.FirstName, entity.MiddleName, entity.LastName, entity.SecondLastName }
             .Where(x => !string.IsNullOrWhiteSpace(x)));
 
-    private static string GetContactValue(ProfileEntity entity, string contactTypeName) =>
-        entity.Resume.ContactInfo
+    private static string GetContactValue(ProfileEntity entity, string contactTypeName)
+    {
+        return entity.Resume.ContactInfo
             .FirstOrDefault(c => c.ContactType.Name == contactTypeName)
             ?.Value ?? string.Empty;
+    }
 }
