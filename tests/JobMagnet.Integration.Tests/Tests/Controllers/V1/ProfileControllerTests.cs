43759d9c012e0f5c55113e0a79d6f2775d9ea3fb
Henk Sandoval <henk.sandoval@hotmail.com>
2025-04-18 15:16:31 +0200

    test: 🧪 Add assertions in ProfileControllerTests to validate PersonalData, About, Testimonials, and SkillSet

--- tests/JobMagnet.Integration.Tests/Tests/Controllers/V1/ProfileControllerTests.cs
+++ tests/JobMagnet.Integration.Tests/Tests/Controllers/V1/ProfileControllerTests.cs
@@ -52,6 +52,11 @@ public class ProfileControllerTests : IClassFixture<JobMagnetTestSetupFixture>
         var responseData = await TestUtilities.DeserializeResponseAsync<ProfileViewModel>(response);
         responseData.ShouldNotBeNull();
         responseData.ShouldBeAssignableTo<ProfileViewModel>();
+
+        responseData.PersonalData.ShouldNotBeNull();
+        responseData.About.ShouldNotBeNull();
+        responseData.Testimonials.ShouldNotBeNull();
+        responseData.SkillSet.ShouldNotBeNull();
     }
 
     private async Task<ProfileEntity> SetupEntityAsync()
