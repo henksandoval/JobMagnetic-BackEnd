605ab5bcdd5bf526ccb6e84d48e9099b22de5062
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-04-18 22:37:17 +0200

    feat: ✨ Add new image entries to SeedData for web and brand graphic design and SEO consulting

--- sources/JobMagnet/Infrastructure/Seeders/SeedData.cs
+++ sources/JobMagnet/Infrastructure/Seeders/SeedData.cs
@@ -436,7 +436,35 @@ public static class SeedData
                  UrlLink = "https://example.com/ui-ux-design",
                  UrlImage = "https://cdn.pixabay.com/photo/2023/08/11/08/29/highland-cattle-8183107_1280.jpg",
                  UrlVideo = "https://example.com/video2.mp4",
-                 Type = "video",
+                 Type = "image",
+                 ServiceId = serviceEntity.Id,
+                 AddedAt = DateTime.Now,
+                 AddedBy = Guid.Empty
+             },
+             new()
+             {
+                 Id = 0,
+                 Position = 3,
+                 Title = "Web and Brand Graphic Design",
+                 Description = "Creating intuitive and engaging user interfaces.",
+                 UrlLink = "https://example.com/ux-design",
+                 UrlImage = "https://cdn.pixabay.com/photo/2024/02/20/13/21/mountains-8585535_1280.jpg",
+                 UrlVideo = "https://example.com/video3.mp4",
+                 Type = "image",
+                 ServiceId = serviceEntity.Id,
+                 AddedAt = DateTime.Now,
+                 AddedBy = Guid.Empty
+             },
+             new()
+             {
+                 Id = 0,
+                 Position = 4,
+                 Title = "SEO Consulting",
+                 Description = "Creating intuitive and engaging user interfaces.",
+                 UrlLink = "https://example.com/ui-ux-design2",
+                 UrlImage = "https://cdn.pixabay.com/photo/2024/01/25/10/50/mosque-8531576_1280.jpg",
+                 UrlVideo = "https://example.com/video4.mp4",
+                 Type = "image",
                  ServiceId = serviceEntity.Id,
                  AddedAt = DateTime.Now,
                  AddedBy = Guid.Empty
