307713fc3a3b449328049d3c8b01343839f8faef
Henk Sandoval <henk.sandoval@hotmail.com>
2025-04-18 20:05:54 +0200

    feat: ✨ Add portfolio registration to SeedData for initial setup

--- sources/JobMagnet/Infrastructure/Seeders/SeedData.cs
+++ sources/JobMagnet/Infrastructure/Seeders/SeedData.cs
@@ -194,7 +194,8 @@ public static class SeedData
             RegisterTalentsAsync(context, profile.Id),
             RegisterResumeAsync(context, profile.Id),
             RegisterTestimonialAsync(context, profile.Id),
-            RegisterSkillAsync(context, profile.Id)
+            RegisterSkillAsync(context, profile.Id),
+            RegisterPortfolioAsync(context, profile.Id)
         };
 
         await Task.WhenAll(tasks);
@@ -494,6 +495,119 @@ public static class SeedData
         };
 
         await context.Skills.AddAsync(skillEntity);
+    }
+
+    private static async Task RegisterPortfolioAsync(JobMagnetDbContext context, long profileId)
+    {
+        if (context.PortfolioGalleries.Any()) return;
+
+        var portfolioEntities = new PortfolioGalleryEntity[]
+        {
+            new()
+            {
+                Id = 0,
+                Position = 1,
+                Title = "Aventuras Animales",
+                Description = "Cada fotografía captura momentos únicos y comportamientos fascinantes.",
+                UrlLink = "https://waylet.es/",
+                UrlImage = "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg",
+                Type = "CAT",
+                UrlVideo = string.Empty,
+                ProfileId = profileId,
+                AddedAt = DateTime.Now,
+                AddedBy = Guid.Empty
+            },
+            new()
+            {
+                Id = 0,
+                Position = 2,
+                Title = "Horizontes Naturales",
+                Description =
+                    "Cada imagen captura la esencia de lugares únicos, desde montañas imponentes hasta costas tranquilas, invitándote a explorar la belleza del mundo",
+                UrlLink = "https://biati-digital.github.io/glightbox/",
+                UrlImage = "https://th.bing.com/th/id/OIP.iwFhHHKPOqAJUDO-iSov_wHaE8?rs=1&pid=ImgDetMain",
+                Type = "NATURE",
+                UrlVideo = string.Empty,
+                ProfileId = profileId,
+                AddedAt = DateTime.Now,
+                AddedBy = Guid.Empty
+            },
+            new()
+            {
+                Id = 0,
+                Position = 3,
+                Title = "Movil Truck",
+                Description =
+                    "Plataforma de transporte inteligente; solución tecnológica diseñada para abordar de manera eficiente el transporte de mercancías por carretera.",
+                UrlLink = "https://moviltruck.com/",
+                UrlImage = "https://moviltruck.com/wp-content/uploads/2023/11/Hero-1-.png",
+                Type = "WebPage",
+                UrlVideo = string.Empty,
+                ProfileId = profileId,
+                AddedAt = DateTime.Now,
+                AddedBy = Guid.Empty
+            },
+            new()
+            {
+                Id = 0,
+                Position = 4,
+                Title = "Aventuras Animales",
+                Description = "Cada fotografía captura momentos únicos y comportamientos fascinantes.",
+                UrlLink = string.Empty,
+                UrlImage = "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg",
+                Type = "CAT",
+                UrlVideo = string.Empty,
+                ProfileId = profileId,
+                AddedAt = DateTime.Now,
+                AddedBy = Guid.Empty
+            },
+            new()
+            {
+                Id = 0,
+                Position = 5,
+                Title = "Aventuras Animales",
+                Description = "Cada fotografía captura momentos únicos y comportamientos fascinantes.",
+                UrlLink = string.Empty,
+                UrlImage = "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg",
+                Type = "CAT",
+                UrlVideo = string.Empty,
+                ProfileId = profileId,
+                AddedAt = DateTime.Now,
+                AddedBy = Guid.Empty
+            },
+            new()
+            {
+                Id = 0,
+                Position = 6,
+                Title = "Music",
+                Description =
+                    "Cada imagen captura la esencia de la musica, el sonido llega al alma dando una hermosa sensacion de relajacion",
+                UrlLink = "",
+                UrlImage = "https://i0.wp.com/www.nus.agency/wp-content/uploads/2023/03/musica-arte-scaled.jpg?ssl=1",
+                Type = "Music",
+                UrlVideo = string.Empty,
+                ProfileId = profileId,
+                AddedAt = DateTime.Now,
+                AddedBy = Guid.Empty
+            },
+            new()
+            {
+                Id = 0,
+                Position = 7,
+                Title = "Red And Blue Parrot",
+                Description = "Hermosos y encantadores Guacamayas en ambiente natural.",
+                UrlLink = string.Empty,
+                UrlImage =
+                    "https://images.pexels.com/photos/1427447/pexels-photo-1427447.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                Type = "NATURE",
+                UrlVideo = "https://videos.pexels.com/video-files/17325162/17325162-uhd_1440_2560_30fps.mp4",
+                ProfileId = profileId,
+                AddedAt = DateTime.Now,
+                AddedBy = Guid.Empty
+            }
+        };
+
+        await context.PortfolioGalleries.AddRangeAsync(portfolioEntities);
     }
 
     private static void FillContactInfo(ResumeEntity resumeEntity)
