73272942ce4c4b98890e579d75cb9ddd6212b53c
Henk Sandoval <henk.sandoval@hotmail.com>
2025-04-21 07:37:32 +0200

    test: 🧪 Add integration tests for AdminController with ping endpoint validation

---
+++ tests/JobMagnet.Integration.Tests/Tests/Controllers/V0/AdminControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using JobMagnet.Integration.Tests.Fixtures;
+using Shouldly;
+using Xunit.Abstractions;
+
+namespace JobMagnet.Integration.Tests.Tests.Controllers.V0;
+
+public class AdminControllerTests : IClassFixture<JobMagnetTestSetupFixture>
+{
+    private const string RequestUriController = "api/v0.1/admin";
+    private readonly HttpClient _httpClient;
+
+    public AdminControllerTests(JobMagnetTestSetupFixture testFixture, ITestOutputHelper testOutputHelper)
+    {
+        _httpClient = testFixture.GetClient();
+        testFixture.SetTestOutputHelper(testOutputHelper);
+    }
+
+    [Fact(DisplayName = "Should return 200 and Pong when GET ping request is called")]
+    public async Task ShouldReturnPong_WhenGetPingRequestAsync()
+    {
+        // When
+        var response = await _httpClient.GetAsync($"{RequestUriController}/ping");
+
+        // Then
+        response.IsSuccessStatusCode.ShouldBeTrue();
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        var responseData = await response.Content.ReadAsStringAsync();
+        responseData.ShouldNotBeNullOrEmpty();
+        responseData.ShouldBe("Pong");
+    }
+}
