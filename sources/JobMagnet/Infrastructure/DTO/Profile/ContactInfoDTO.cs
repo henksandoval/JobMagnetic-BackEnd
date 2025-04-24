150367488bc27c086242688aa2a570d14f2ff3b8
Henk Sandoval <henk.sandoval@hotmail.com>
2025-04-24 17:04:15 +0200

    revert: 🔥 Remove DTOs

--- sources/JobMagnet/Infrastructure/DTO/Profile/ContactInfoDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JobMagnet.Infrastructure.DTO.Profile;
-
-public class ContactInfoDTO
-{
-    public required string Value { get; set; }
-
-    public virtual ContactTypeDTO ContactType { get; set; }
-    public virtual ResumeDTO Resume { get; set; }
-}
--- sources/JobMagnet/Infrastructure/DTO/Profile/TalentDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JobMagnet.Infrastructure.DTO.Profile;
-
-public class TalentDTO
-{
-    public required string Description { get; set; }
-
-    public virtual ProfileDTO Profile { get; set; }
-}
