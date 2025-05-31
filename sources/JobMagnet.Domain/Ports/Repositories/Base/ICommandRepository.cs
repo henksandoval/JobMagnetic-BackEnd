c60f9e2fb8b2c9fe4baa4582ccc14439ec842dc4
Henk Sandoval <henk.sandoval@hotmail.com>
2025-05-31 07:25:15 +0200

    refactor: 🚚 Clean up project references and remove unused transaction methods

--- sources/JobMagnet.Domain/Ports/Repositories/Base/ICommandRepository.cs
+++ sources/JobMagnet.Domain/Ports/Repositories/Base/ICommandRepository.cs
@@ -1,12 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore.Storage;
-
-namespace JobMagnet.Domain.Domain.Ports.Repositories.Base;
+﻿namespace JobMagnet.Domain.Domain.Ports.Repositories.Base;
 
 public interface ICommandRepository<in TEntity> where TEntity : class
 {
     Task CreateAsync(TEntity entity);
     Task<bool> UpdateAsync(TEntity entity);
-    Task<IDbContextTransaction> BeginTransactionAsync();
-    Task CommitAsync(IDbContextTransaction transaction);
     Task<bool?> HardDeleteAsync(TEntity entity);
 }
