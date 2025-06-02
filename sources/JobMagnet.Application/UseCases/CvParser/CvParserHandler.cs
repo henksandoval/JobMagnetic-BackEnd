80d4310a0f9705ed632615f15f0ad9245ae3b8ee
Henk Sandoval <henk.sandoval@hotmail.com>
2025-06-02 08:23:33 +0200

    feat: ✨ Refactor CvParserHandler to separate parsing and persistence logic, and enhance contact type resolution

--- sources/JobMagnet.Application/UseCases/CvParser/CvParserHandler.cs
+++ sources/JobMagnet.Application/UseCases/CvParser/CvParserHandler.cs
@@ -25,24 +25,33 @@ public class CvParserHandler(
     public async Task ParseAsync(CvParserCommand command, CancellationToken cancellationToken = default)
     {
         ArgumentNullException.ThrowIfNull(command);
+        var profileEntity = await ParseCvToProfileEntity(command);
+        await PersistProfileAsync(profileEntity, cancellationToken);
+    }
 
+    private async Task<ProfileEntity> ParseCvToProfileEntity(CvParserCommand command)
+    {
+        var rawProfile = await _cvParser.ParseAsync(command.Stream);
+
+        if (rawProfile.HasNoValue)
+        {
+            throw new JobMagnetApplicationException("Failed to parse the CV. The raw profile is empty.");
+        }
+
+        var parsedProfileDto = rawProfile.Value.ToProfileParseDto();
+        var profileEntity = parsedProfileDto.ToProfileEntity();
+        return profileEntity;
+    }
+
+    private async Task PersistProfileAsync(ProfileEntity profileEntity, CancellationToken cancellationToken)
+    {
         try
         {
             await _unitOfWork.BeginTransactionAsync(cancellationToken);
 
-            var rawProfile = await _cvParser.ParseAsync(command.Stream);
-
-            if (rawProfile.HasNoValue)
-            {
-                throw new JobMagnetApplicationException("Failed to parse the CV. The raw profile is empty.");
-            }
-
-            var parsedProfileDto = rawProfile.Value.ToProfileParseDto();
-            var profileEntity = parsedProfileDto.ToProfileEntity();
-
             SetAuditingFields(profileEntity);
 
-            if (profileEntity.Resume?.ContactInfo != null && profileEntity.Resume.ContactInfo.Count != 0)
+            if (profileEntity.Resume?.ContactInfo is { Count: > 0 })
             {
                 await ResolveContactTypesAsync(profileEntity.Resume.ContactInfo, cancellationToken);
             }
@@ -63,10 +72,42 @@ public class CvParserHandler(
     }
 
     private async Task ResolveContactTypesAsync(
-        ICollection<ContactInfoEntity>? contactInfos,
+        ICollection<ContactInfoEntity>? contactInfo,
         CancellationToken cancellationToken = default)
     {
-        //TODO: Implement contact type resolution logic
-        await Task.Delay(0, cancellationToken);
+        if (contactInfo is null || contactInfo.Count == 0)
+        {
+            return;
+        }
+
+        var contactTypesFromDto = contactInfo
+            .Select(inf => inf.ContactType.Name)
+            .Where(name => !string.IsNullOrEmpty(name))
+            .Distinct()
+            .ToList();
+
+        var existingContactTypes = await _contactTypeQueryRepository
+            .FindAsync(type => contactTypesFromDto.Contains(type.Name), cancellationToken)
+            .ConfigureAwait(false);
+
+        var existingContactTypeNamesDictionary = existingContactTypes.ToDictionary(type => type.Name, name => name);
+
+        foreach (var info in contactInfo)
+        {
+            var typeName = info.ContactType.Name;
+
+            if (existingContactTypeNamesDictionary.TryGetValue(typeName, out var existingContactType))
+            {
+                info.ContactType = existingContactType;
+                info.ContactTypeId = existingContactType.Id;
+                continue;
+            }
+
+            info.ContactType = new ContactTypeEntity
+            {
+                Id = 0,
+                Name = typeName
+            };
+        }
     }
 }
