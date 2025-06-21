f3b75851495e0e5cb7f04c1526bee687b7b2b02f
Henk Sandoval <henk.sandoval@outlook.com>
2025-06-21 11:57:10 +0200

    refactor: 🔥 Update namespace for CvParserException to improve code organization

--- sources/JobMagnet.Infrastructure/Services/CvParsers/Exceptions/CvParserException.cs
+++ sources/JobMagnet.Infrastructure/Services/CvParsers/Exceptions/CvParserException.cs
@@ -1,3 +1,3 @@
-namespace JobMagnet.Infrastructure.ExternalServices.CvParsers.Exceptions;
+namespace JobMagnet.Infrastructure.Services.CvParsers.Exceptions;
 
 internal class CvParserException(string message, Exception innerException) : Exception(message, innerException);
