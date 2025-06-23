17fff9994fc315967caf158818a84644f875ebf0
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-06-23 15:02:54 +0200

    feat: ✨ Refactor TalentEntity to encapsulate properties and enforce validation in constructor

--- sources/JobMagnet.Domain/Core/Entities/TalentEntity.cs
+++ sources/JobMagnet.Domain/Core/Entities/TalentEntity.cs
@@ -1,11 +1,28 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using CommunityToolkit.Diagnostics;
 using JobMagnet.Domain.Core.Entities.Base;
 
 namespace JobMagnet.Domain.Core.Entities;
 
 public class TalentEntity : SoftDeletableEntity<long>
 {
-    public required string Description { get; set; }
-    [ForeignKey(nameof(Profile))] public long ProfileId { get; set; }
-    public virtual ProfileEntity Profile { get; set; }
+    public string Description { get; private set; } 
+    public long ProfileId { get; private set; }
+    public virtual ProfileEntity Profile { get; private set; }
+    private TalentEntity() { }
+
+    [SetsRequiredMembers]
+    internal TalentEntity(string description, long profileId, ProfileEntity profileEntity, long id = 0)
+    {
+        Guard.IsGreaterThanOrEqualTo(id, 0);
+        Guard.IsGreaterThanOrEqualTo(profileId, 0);
+        Guard.IsNotNullOrWhiteSpace(description);
+        Guard.IsNotNull(profileEntity);
+        
+        Id = id;
+        Description = description;
+        ProfileId = profileId;
+        Profile = profileEntity;
+    }
 }
