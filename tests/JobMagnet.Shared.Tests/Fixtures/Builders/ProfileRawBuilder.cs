4487f8e7f902e52a98ab5bb5fbcf958b1b628dd2
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-06-24 17:42:38 +0200

    feat: ✨ Update ProfileRawBuilder to generate talents dynamically with a default count

--- tests/JobMagnet.Shared.Tests/Fixtures/Builders/ProfileRawBuilder.cs
+++ tests/JobMagnet.Shared.Tests/Fixtures/Builders/ProfileRawBuilder.cs
@@ -73,8 +73,9 @@ public class ProfileRawBuilder
         return this;
     }
 
-    public ProfileRawBuilder WithTalents(List<TalentRaw> talents)
+    public ProfileRawBuilder WithTalents(int count = 5)
     {
+        var talents = _fixture.CreateMany<TalentRaw>(count).ToList();
         _instance = _instance with { Talents = talents };
         return this;
     }
