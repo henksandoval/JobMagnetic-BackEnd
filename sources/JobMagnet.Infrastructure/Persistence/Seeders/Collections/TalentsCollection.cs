3c494f10164ddf873a22f07415a37b87bdd3ea76
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-06-24 17:43:46 +0200

    feat: ✨ Simplify Talents record by removing ProfileEntity dependency

--- sources/JobMagnet.Infrastructure/Persistence/Seeders/Collections/TalentsCollection.cs
+++ sources/JobMagnet.Infrastructure/Persistence/Seeders/Collections/TalentsCollection.cs
@@ -4,30 +4,30 @@ using JobMagnet.Domain.Core.Entities;
 namespace JobMagnet.Infrastructure.Persistence.Seeders.Collections;
 
 // ReSharper disable once NotAccessedPositionalProperty.Global
-public record Talents(string Description, long ProfileId, ProfileEntity Profile);
+public record Talents(string Description);
 public record TalentsCollection
 {
     private readonly long _profileId;
 
     private readonly IList<Talents> _values =
     [
-        new ("Creative", 1, new ProfileEntity { Id = 1 }),
-        new ("Problem Solver", 1, new ProfileEntity { Id = 1 }),
-        new ("Detail Oriented", 1, new ProfileEntity { Id = 1 }),
-        new ("Excellent Communicator", 1, new ProfileEntity { Id = 1 }),
-        new ("Adaptable", 1, new ProfileEntity { Id = 1 }),
-        new ("Analytical Thinker", 1, new ProfileEntity { Id = 1 }),
-        new ("Innovative", 1, new ProfileEntity { Id = 1 }),
-        new ("Self-Motivated", 1, new ProfileEntity { Id = 1 }),
-        new ("Collaborative", 1, new ProfileEntity { Id = 1 }),
-        new ("Time Management", 1, new ProfileEntity { Id = 1 }),
-        new ("Critical Thinking", 1, new ProfileEntity { Id = 1 }),
-        new ("Leadership Skills", 1, new ProfileEntity { Id = 1 }),
-        new ("Technical Proficiency", 1, new ProfileEntity { Id = 1 }),
-        new ("Project Management", 1, new ProfileEntity { Id = 1 }),
-        new ("Customer Focused", 1, new ProfileEntity { Id = 1 }),
-        new ("Team Player", 1, new ProfileEntity { Id = 1 }),
-        new ("Fast Learner", 1, new ProfileEntity { Id = 1 })
+        new ("Creative"),
+        new ("Problem Solver"),
+        new ("Detail Oriented"),
+        new ("Excellent Communicator"),
+        new ("Adaptable"),
+        new ("Analytical Thinker"),
+        new ("Innovative"),
+        new ("Self-Motivated"),
+        new ("Collaborative"),
+        new ("Time Management"),
+        new ("Critical Thinking"),
+        new ("Leadership Skills"),
+        new ("Technical Proficiency"),
+        new ("Project Management"),
+        new ("Customer Focused"),
+        new ("Team Player"),
+        new ("Fast Learner"),
     ];
 
     public TalentsCollection(long profileId = 0)
@@ -40,8 +40,6 @@ public record TalentsCollection
         return _values.Select(talent => new TalentEntity
             (
                 talent.Description,
-                talent.ProfileId, 
-                talent.Profile,
                 _profileId
             )).ToImmutableList();
     }
