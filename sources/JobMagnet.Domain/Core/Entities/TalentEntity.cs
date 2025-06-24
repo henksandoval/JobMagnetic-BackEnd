18c66caa738493f673ee1946f6226218f060c56d
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-06-24 17:43:33 +0200

    feat: ✨ Update TalentEntity constructor to remove ProfileEntity dependency and simplify parameters

--- sources/JobMagnet.Domain/Core/Entities/TalentEntity.cs
+++ sources/JobMagnet.Domain/Core/Entities/TalentEntity.cs
@@ -9,20 +9,17 @@ public class TalentEntity : SoftDeletableEntity<long>
 {
     public string Description { get; private set; } 
     public long ProfileId { get; private set; }
-    public virtual ProfileEntity Profile { get; private set; }
     private TalentEntity() { }
 
     [SetsRequiredMembers]
-    internal TalentEntity(string description, long profileId, ProfileEntity profileEntity, long id = 0)
+    public TalentEntity(string description, long profileId = 0,  long id = 0)
     {
         Guard.IsGreaterThanOrEqualTo(id, 0);
         Guard.IsGreaterThanOrEqualTo(profileId, 0);
         Guard.IsNotNullOrWhiteSpace(description);
-        Guard.IsNotNull(profileEntity);
         
         Id = id;
         Description = description;
         ProfileId = profileId;
-        Profile = profileEntity;
     }
 }
