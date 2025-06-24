a77c8829f7b856108c4c90ed3f4e802aff307841
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-06-24 12:31:26 +0200

    feat: ✨ Enhance TalentsCollection to use a list of Talents with detailed attributes

--- sources/JobMagnet.Infrastructure/Persistence/Seeders/Collections/TalentsCollection.cs
+++ sources/JobMagnet.Infrastructure/Persistence/Seeders/Collections/TalentsCollection.cs
@@ -4,32 +4,45 @@ using JobMagnet.Domain.Core.Entities;
 namespace JobMagnet.Infrastructure.Persistence.Seeders.Collections;
 
 // ReSharper disable once NotAccessedPositionalProperty.Global
+public record Talents(string Description, long ProfileId, ProfileEntity Profile);
 public record TalentsCollection
 {
     private readonly long _profileId;
 
-    private readonly string[] _values =
-    {
-        "Creative",
-        "Problem Solver",
-        "Team Player",
-        "Fast Learner"
-    };
+    private readonly IList<Talents> _values =
+    [
+        new ("Creative", 1, new ProfileEntity { Id = 1 }),
+        new ("Problem Solver", 1, new ProfileEntity { Id = 1 }),
+        new ("Detail Oriented", 1, new ProfileEntity { Id = 1 }),
+        new ("Excellent Communicator", 1, new ProfileEntity { Id = 1 }),
+        new ("Adaptable", 1, new ProfileEntity { Id = 1 }),
+        new ("Analytical Thinker", 1, new ProfileEntity { Id = 1 }),
+        new ("Innovative", 1, new ProfileEntity { Id = 1 }),
+        new ("Self-Motivated", 1, new ProfileEntity { Id = 1 }),
+        new ("Collaborative", 1, new ProfileEntity { Id = 1 }),
+        new ("Time Management", 1, new ProfileEntity { Id = 1 }),
+        new ("Critical Thinking", 1, new ProfileEntity { Id = 1 }),
+        new ("Leadership Skills", 1, new ProfileEntity { Id = 1 }),
+        new ("Technical Proficiency", 1, new ProfileEntity { Id = 1 }),
+        new ("Project Management", 1, new ProfileEntity { Id = 1 }),
+        new ("Customer Focused", 1, new ProfileEntity { Id = 1 }),
+        new ("Team Player", 1, new ProfileEntity { Id = 1 }),
+        new ("Fast Learner", 1, new ProfileEntity { Id = 1 })
+    ];
 
     public TalentsCollection(long profileId = 0)
     {
         _profileId = profileId;
     }
 
-    // public ImmutableList<TalentEntity> GetTalents()
-    // {
-    //     return _values.Select(talent => new TalentEntity
-    //     {
-    //         Id = 0,
-    //         Description = talent,
-    //         ProfileId = _profileId,
-    //         AddedAt = DateTime.Now,
-    //         AddedBy = Guid.Empty
-    //     }).ToImmutableList();
-    // }
+    public ImmutableList<TalentEntity> GetTalents()
+    {
+        return _values.Select(talent => new TalentEntity
+            (
+                talent.Description,
+                talent.ProfileId, 
+                talent.Profile,
+                _profileId
+            )).ToImmutableList();
+    }
 }
