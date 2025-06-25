e7f513a0cbc5375821ff7ca1a298736ff36662f0
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-06-25 20:07:14 +0200

    feat: ✨ Implement TalentShowcase to manage talents for a profile

---
+++ sources/JobMagnet.Domain/Core/Entities/TalentShowcase.cs
@@ -0,0 +1,30 @@
+using CommunityToolkit.Diagnostics;
+using JobMagnet.Domain.Exceptions;
+
+namespace JobMagnet.Domain.Core.Entities;
+
+public class TalentShowcase
+{
+    private readonly ProfileEntity _profile;
+    private IReadOnlyCollection<TalentEntity> Talents => _profile.Talents;
+    
+    internal TalentShowcase(ProfileEntity profile)
+    {
+        Guard.IsNotNull(profile);
+        _profile = profile;
+    }
+    public void AddTalent(string description)
+    {
+        if (Talents.Count >= 10)
+        {
+            throw new JobMagnetDomainException("Cannot add more than 10 talents.");
+        }
+        if (Talents.Any(t => t.Description == description))
+        {
+            throw new JobMagnetDomainException("This talent already exists.");
+        }
+        var talent = new TalentEntity(description, _profile.Id);
+        
+        _profile.AddTalent(talent.Description);
+    }
+}
