e5be3e8ca9e8cd535de7b44026673f9b72873b91
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-07-02 23:25:02 +0200

    feat: ✨ Add GetTalentsByProfileAsync method and update route in ProfileController

--- sources/JobMagnet.Host/Controllers/V1/ProfileController.Talent.cs
+++ sources/JobMagnet.Host/Controllers/V1/ProfileController.Talent.cs
@@ -1,7 +1,7 @@
 using JobMagnet.Application.Contracts.Commands.Talent;
+using JobMagnet.Application.Contracts.Responses.TalentShowcase;
 using JobMagnet.Application.Mappers;
 using JobMagnet.Domain.Aggregates.Profiles;
-using JobMagnet.Domain.Aggregates.Profiles.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JobMagnet.Host.Controllers.V1;
@@ -10,7 +10,6 @@ public partial class ProfileController
 {
     [HttpPost("{profileId:guid}/talents")]
     [ProducesResponseType(StatusCodes.Status201Created)]
-    
     public async Task<IResult> AddTalentsToProfileAsync(Guid profileId, [FromBody] TalentCommand command, CancellationToken cancellationToken)
     {
         if (profileId != command.TalentData?.ProfileId)
@@ -24,14 +23,21 @@ public partial class ProfileController
         var talent = profile.TalentShowcase.AddTalent(
             command.TalentData.Description ?? string.Empty
         );
-        
-        await talentCommandRepository.CreateAsync(talent, cancellationToken);
+
+        profileCommandRepository.Update(profile);
         await unitOfWork.SaveChangesAsync(cancellationToken);
         
         var result = talent.ToModel();
         
-        return Results.CreatedAtRoute("GettalentById", new { profileId = profile.Id }, result);
+        return Results.CreatedAtRoute("GetTalentsByProfile", new { profileId }, result);
+    }
 
+    [HttpGet("{profileId:guid}/talents", Name = "GetTalentsByProfile")]
+    [ProducesResponseType(typeof(IEnumerable<TalentResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IResult> GetTalentsByProfileAsync(Guid profileId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
     }
 
     private async Task<Profile?> GetProfileWithTalent(Guid profileId, CancellationToken cancellationToken)
