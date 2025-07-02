1a65874ae5b4364227dc9562e3abb769b82ea82c
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-07-02 23:25:44 +0200

    feat: ✨ Move Talent to ValueObjects namespace and add CreateInstance method

--- sources/JobMagnet.Domain/Aggregates/Profiles/ValueObjects/Talent.cs
+++ sources/JobMagnet.Domain/Aggregates/Profiles/ValueObjects/Talent.cs
@@ -1,19 +1,26 @@
 ﻿using CommunityToolkit.Diagnostics;
 
-namespace JobMagnet.Domain.Aggregates.Profiles.Entities;
+namespace JobMagnet.Domain.Aggregates.Profiles.ValueObjects;
 
 public record Talent
 {
     public const int MaxNameLength = 50;
+    public Guid Id { get; private set; }
+    public ProfileId ProfileId { get; private set; }
     public string Description { get; private set; }
-
+    
     private Talent() { }
 
-    public Talent(string description)
+    private Talent(string description)
     {
         Guard.IsNotNullOrWhiteSpace(description);
         Guard.IsLessThanOrEqualTo(description.Length, MaxNameLength);
 
         Description = description;
+    }
+
+    public static Talent CreateInstance(string description)
+    {
+        return new Talent(description);
     }
 }
