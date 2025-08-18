0f46b3ad613a46dd93c343d52d14c6560880ec04
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-08-18 17:43:30 +0200

    feat: ✨ Configure Identity and JWT authentication in Program.cs

--- sources/JobMagnet.Host/Program.cs
+++ sources/JobMagnet.Host/Program.cs
@@ -1,9 +1,13 @@
+using System.Text;
 using JobMagnet.Application.Extensions;
 using JobMagnet.Application.Services;
 using JobMagnet.Host.Extensions;
 using JobMagnet.Host.Services;
 using JobMagnet.Infrastructure.Extensions;
 using JobMagnet.Infrastructure.Persistence.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +30,21 @@ builder.Services
     .AddConfiguredOpenApi(builder.Configuration)
     .AddSwagger()
     .AddControllers();
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<JobMagnetDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["jwt:Key"] ?? string.Empty)),
+        ClockSkew = TimeSpan.Zero
+    });
 
 var app = builder.Build();
 
