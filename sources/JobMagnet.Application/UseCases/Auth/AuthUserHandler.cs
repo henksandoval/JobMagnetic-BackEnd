10e90e5838314301385e1ac8e754bbea19068467
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-08-18 12:47:54 +0200

    feat: ✨ Remove unused Microsoft.AspNetCore.Identity reference from AuthUserHandler

--- sources/JobMagnet.Application/UseCases/Auth/AuthUserHandler.cs
+++ sources/JobMagnet.Application/UseCases/Auth/AuthUserHandler.cs
@@ -1,6 +1,5 @@
 using JobMagnet.Application.UseCases.Auth.DTO;
 using JobMagnet.Application.UseCases.Auth.Interface;
-using Microsoft.AspNetCore.Identity;
 
 namespace JobMagnet.Application.UseCases.Auth;
 
