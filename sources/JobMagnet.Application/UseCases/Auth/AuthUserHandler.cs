36f7d55c17a2b9d42e0d3d888b9105f39df4346b
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-08-18 12:45:56 +0200

    feat: ✨ Add AuthUserHandler and IAuthUserHandler for user authentication handling

---
+++ sources/JobMagnet.Application/UseCases/Auth/AuthUserHandler.cs
@@ -0,0 +1,13 @@
+using JobMagnet.Application.UseCases.Auth.DTO;
+using JobMagnet.Application.UseCases.Auth.Interface;
+using Microsoft.AspNetCore.Identity;
+
+namespace JobMagnet.Application.UseCases.Auth;
+
+public class AuthUserHandler : IAuthUserHandler
+{
+
+    public AuthUserHandler()
+    {
+    }
+}
---
+++ sources/JobMagnet.Application/UseCases/Auth/Interface/IAuthUserHandler.cs
@@ -0,0 +1,7 @@
+using JobMagnet.Application.UseCases.Auth.DTO;
+
+namespace JobMagnet.Application.UseCases.Auth.Interface;
+
+public interface IAuthUserHandler
+{
+}
