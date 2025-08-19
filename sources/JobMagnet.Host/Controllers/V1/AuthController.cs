a8edb38fe6c8bdc7911a1368d16f9ce36244a890
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-08-19 14:45:49 +0200

    feat: ✨ Implement AuthController with login endpoint and unit tests

---
+++ sources/JobMagnet.Host/Controllers/V1/AuthController.cs
@@ -0,0 +1,18 @@
+using Asp.Versioning;
+using JobMagnet.Application.UseCases.Auth.DTO;
+using JobMagnet.Application.UseCases.Auth.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace JobMagnet.Host.Controllers.V1;
+
+[ApiVersion("1")]
+public class AuthController(IAuthUserHandler handler)
+{
+    [HttpPost("login", Name = "loginUser")]
+    public async Task<IResult> Login([FromBody] LoginDto loginRequest)
+    {
+        var token = await handler.LoginAsync(loginRequest);
+        return Results.Ok(token);
+    }
+}
---
+++ tests/JobMagnet.Unit.Tests/AuthControllerShould.cs/AuthController.cs
@@ -0,0 +1,46 @@
+using System.Text.Json;
+using AutoFixture;
+using AwesomeAssertions;
+using JobMagnet.Application.UseCases.Auth.DTO;
+using JobMagnet.Application.UseCases.Auth.Interface;
+using JobMagnet.Host.Controllers.V1;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace JobMagnet.Unit.Tests.AuthControllerShould.cs;
+
+public class AuthControllerShould
+{
+    private readonly IFixture _fixture;
+    private readonly Mock<IAuthUserHandler> _handlerMock; 
+    private readonly AuthController _controller;
+    
+    public AuthControllerShould()
+    {
+        _fixture = new Fixture();
+        _handlerMock = new Mock<IAuthUserHandler>();
+        _controller = new AuthController(_handlerMock.Object);
+    }
+
+    [Fact]
+    public async Task ReturnToken_WhenCredentialsAreValid()
+    {
+        // --- Given ---
+        var loginDTo = _fixture.Create<LoginDto>();
+        var expectedToken = _fixture.Create<UserToken>();
+        _handlerMock.Setup(h => h.LoginAsync(loginDTo)).ReturnsAsync(expectedToken);
+        
+        // --- When ---
+        var result = await _controller.Login(loginDTo);
+        
+        // --- Then ---
+        var currentToken = result.Should()
+            .BeAssignableTo<Ok<UserToken>>()
+            .Subject.Value;
+        
+        currentToken.Should().BeEquivalentTo(expectedToken);
+    }
+}
