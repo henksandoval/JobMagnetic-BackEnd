dcab03a5bf16c7a5ade9794ab3f4af4ca9e653d4
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-08-25 12:05:40 +0200

    feat: ✨ Enhance AuthUserHandler tests with CreateAdminUserAsync scenario and use AutoFixture for LoginDto

--- tests/JobMagnet.Unit.Tests/UseCases/AuthUserHandlerShould.cs
+++ tests/JobMagnet.Unit.Tests/UseCases/AuthUserHandlerShould.cs
@@ -1,31 +1,41 @@
+using AutoFixture;
 using AwesomeAssertions;
 using JobMagnet.Application.UseCases.Auth;
 using JobMagnet.Application.UseCases.Auth.DTO;
-using JobMagnet.Application.UseCases.Auth.Interface;
 using JobMagnet.Application.UseCases.Auth.Ports;
+using JobMagnet.Domain.Aggregates;
 using Moq;
 
 namespace JobMagnet.Unit.Tests.UseCases;
 
 public class AuthUserHandlerShould
 {
-    private readonly Mock<IUserManagerAdapter> _userAdapter = new();
+    private readonly IFixture _fixture;
+    private readonly Mock<IUserManagerAdapter> _userManagerAdapterMock;
+    private readonly AuthUserHandler _authUserHandler;
+
+    public AuthUserHandlerShould()
+    {
+        _fixture = new Fixture();
+        _userManagerAdapterMock = new Mock<IUserManagerAdapter>();
+        _authUserHandler =  new AuthUserHandler(_userManagerAdapterMock.Object);
+    }
     
     [Fact]
     public async Task LoginAsync_WhenCredentialsAreValid_ReturnsUserToken()
     {
         // --- Given ---
-        var loginDto = new LoginDto { Email = "test@email.com", Password = "password" };
+        var loginDto = _fixture.Create<LoginDto>();
         var expectedToken = new UserToken
         {
             Token = "un_jwt_token_valido",
             Expiration = DateTime.UtcNow.AddHours(1)
         };
 
-        _userAdapter.Setup(x => x.LoginAsync(loginDto))
+        _userManagerAdapterMock.Setup(x => x.LoginAsync(loginDto))
             .ReturnsAsync(expectedToken);
     
-        var handler = new AuthUserHandler(_userAdapter.Object);
+        var handler = new AuthUserHandler(_userManagerAdapterMock.Object);
 
         // --- When  ---
         var result = await handler.LoginAsync(loginDto);
@@ -39,9 +49,9 @@ public class AuthUserHandlerShould
     public async Task LoginAsync_WhenAdapterReturnsNull_ReturnsNull()
     {
         // --- Given ---
-        var loginDto = new LoginDto { Email = "test@email.com", Password = "password" };
-        var handler = new AuthUserHandler(_userAdapter.Object);
-        _userAdapter.Setup(x => x.LoginAsync(It.IsAny<LoginDto>())).ReturnsAsync((UserToken)null);
+        var loginDto = _fixture.Create<LoginDto>();
+        var handler = new AuthUserHandler(_userManagerAdapterMock.Object);
+        _userManagerAdapterMock.Setup(x => x.LoginAsync(It.IsAny<LoginDto>())).ReturnsAsync((UserToken)null);
 
         // --- When ---
         var result = await handler.LoginAsync(loginDto);
@@ -61,7 +71,7 @@ public class AuthUserHandlerShould
     public async Task LoginAsync_WhenEmailOrPasswordIsEmpty_ThrowsArgumentException(string email, string password)
     {
         // --- Given ---
-        var handler = new AuthUserHandler(_userAdapter.Object);
+        var handler = new AuthUserHandler(_userManagerAdapterMock.Object);
         var loginDto = new LoginDto { Email = email, Password = password };
         
         // --- When ---
@@ -70,6 +80,26 @@ public class AuthUserHandlerShould
         // --- Then ---
         await action.Should().ThrowAsync<ArgumentException>()
             .WithMessage("The email and password cannot be null, empty, or contain only spaces.");
-      
+    }
+    
+    [Fact]
+    public async Task CreateAdminUserAsync_WhenCalled_ReturnsUserToken()
+    {
+        // --- Given ---
+        var expectedToken = new UserToken
+        {
+            Token = "token_admin",
+            Expiration = DateTime.UtcNow.AddHours(1)
+        };
+        _userManagerAdapterMock.Setup(x => x.CreateAdminUserAsync(It.IsAny<AdminUser>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expectedToken);
+        
+        // --- When ---
+        var result = await _authUserHandler.CreateAdminUserAsync(CancellationToken.None);
+
+        // --- Then ---
+        result.Should().NotBeNull();
+        result.Token.Should().BeEquivalentTo(expectedToken.Token);
+        result.Expiration.Should().BeCloseTo(expectedToken.Expiration, TimeSpan.FromSeconds(1));
     }
 }
