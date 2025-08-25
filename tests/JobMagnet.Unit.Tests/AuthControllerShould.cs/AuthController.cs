2cd517c84646e0bee34830ae93ff02386a10d7a9
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-08-25 12:05:32 +0200

    feat: ✨ Add unit tests for AuthController including unauthorized login and admin user creation scenarios

--- tests/JobMagnet.Unit.Tests/AuthControllerShould.cs/AuthController.cs
+++ tests/JobMagnet.Unit.Tests/AuthControllerShould.cs/AuthController.cs
@@ -4,6 +4,7 @@ using JobMagnet.Application.UseCases.Auth.DTO;
 using JobMagnet.Application.UseCases.Auth.Interface;
 using JobMagnet.Host.Controllers.V1;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 
 namespace JobMagnet.Unit.Tests.AuthControllerShould.cs;
@@ -19,6 +20,20 @@ public class AuthControllerShould
         _fixture = new Fixture();
         _handlerMock = new Mock<IAuthUserHandler>();
         _controller = new AuthController(_handlerMock.Object);
+    }
+    
+    [Fact]
+    public async Task ReturnUnauthorized_WhenCredentialsAreInvalid()
+    {
+        // --- Given ---
+        var loginDto = _fixture.Create<LoginDto>();
+        _handlerMock.Setup(h => h.LoginAsync(loginDto)).ReturnsAsync((UserToken)null);
+
+        // --- When ---
+        var result = await _controller.LoginAsync(loginDto);
+
+        // --- Then ---
+        result.Should().BeOfType<UnauthorizedHttpResult>();
     }
 
     [Fact]
@@ -38,5 +53,23 @@ public class AuthControllerShould
             .Subject.Value;
         
         currentToken.Should().BeEquivalentTo(expectedToken);
+    }
+    
+    [Fact]
+    public async Task CreateAdminUser_ReturnsOk_WhenSuccess()
+    {
+        // --- Given ---
+        var expectedToken =  _fixture.Create<UserToken>();
+        _handlerMock.Setup(h => h.CreateAdminUserAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expectedToken);
+        
+        // --- When ---
+        var controller =await  _controller.CreateAdminUser(CancellationToken.None);
+
+        // --- Then ---
+        var okToken = controller.Should().BeAssignableTo<Ok<UserToken>>()
+            .Subject.Value;
+        
+        okToken.Should().BeEquivalentTo(expectedToken);
     }
 }
