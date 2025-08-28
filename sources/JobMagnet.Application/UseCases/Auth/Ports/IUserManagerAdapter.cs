e6d76c042e1e0039402f8fb51cfc648b91bc224c
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-08-28 10:56:06 +0200

    feat: ✨ Update IUserManagerAdapter to use UserModelCredentials for login and add registration method

--- sources/JobMagnet.Application/UseCases/Auth/Ports/IUserManagerAdapter.cs
+++ sources/JobMagnet.Application/UseCases/Auth/Ports/IUserManagerAdapter.cs
@@ -5,6 +5,7 @@ namespace JobMagnet.Application.UseCases.Auth.Ports;
 
 public interface IUserManagerAdapter
 {
-    Task<UserToken> LoginAsync(LoginDto loginDto);
+    Task<UserToken> LoginAsync(UserModelCredentials userModelCredentials);
     Task<UserToken> CreateAdminUserAsync(AdminUserOptions adminUserOptions,  CancellationToken cancellationToken);
+    Task<UserToken> RegisterAsync(UserModelCredentials userModelCredentials);
 }
