a3cebcaa70161cd3ec2aa0c541641ee9ef7bd556
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-08-28 20:37:10 +0200

    feat: ✨ Add MailSettings class for configuring email server settings

---
+++ sources/JobMagnet.Application/UseCases/Auth/Ports/EmailDTO/MailSettings.cs
@@ -0,0 +1,10 @@
+namespace JobMagnet.Application.UseCases.Auth.Ports.EmailDTO;
+public class MailSettings
+{
+    public string Server { get; init; }
+    public int Port { get; init; }
+    public string SenderName { get; init; }
+    public string SenderEmail { get; init; }
+    public string UserName { get; init; }
+    public string Password { get; init; }
+}
