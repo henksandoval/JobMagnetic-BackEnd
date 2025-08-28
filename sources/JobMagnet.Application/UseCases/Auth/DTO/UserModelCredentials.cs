87333ab4a0013476d52812e445baa0c6729830dd
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-08-28 10:55:37 +0200

    feat: ✨ Rename LoginDto to UserModelCredentials for clarity in authentication handling

--- sources/JobMagnet.Application/UseCases/Auth/DTO/UserModelCredentials.cs
+++ sources/JobMagnet.Application/UseCases/Auth/DTO/UserModelCredentials.cs
@@ -1,6 +1,6 @@
 namespace JobMagnet.Application.UseCases.Auth.DTO;
 
-public class LoginDto
+public class UserModelCredentials
 {
     public string Email { get; set; }
     public string Password { get; set; }
