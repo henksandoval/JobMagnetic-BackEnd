b380b6285dd444fe2275f78cdf5aae8a59528df4
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-08-29 20:43:07 +0200

    feat: ✨ Rename Server property to Host in MailSettings for clarity

--- sources/JobMagnet.Application/UseCases/Auth/Ports/EmailDTO/MailSettings.cs
+++ sources/JobMagnet.Application/UseCases/Auth/Ports/EmailDTO/MailSettings.cs
@@ -1,7 +1,7 @@
 namespace JobMagnet.Application.UseCases.Auth.Ports.EmailDTO;
 public class MailSettings
 {
-    public string Server { get; init; }
+    public string Host { get; init; }
     public int Port { get; init; }
     public string SenderName { get; init; }
     public string SenderEmail { get; init; }
