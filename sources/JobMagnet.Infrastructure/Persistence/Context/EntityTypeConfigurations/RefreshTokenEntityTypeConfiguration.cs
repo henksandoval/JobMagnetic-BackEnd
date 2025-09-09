11aeeef9799c43a5c1140bd15765b76b032f0694
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-09-09 22:26:39 +0200

    feat: ✨ Add RefreshTokenEntityTypeConfiguration for database mapping of RefreshToken entity

---
+++ sources/JobMagnet.Infrastructure/Persistence/Context/EntityTypeConfigurations/RefreshTokenEntityTypeConfiguration.cs
@@ -0,0 +1,24 @@
+using JobMagnet.Application.UseCases.Auth.DTO;
+using JobMagnet.Domain.Aggregates.Auth.Entities;
+using JobMagnet.Domain.Aggregates.Auth.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JobMagnet.Infrastructure.Persistence.Context.EntityTypeConfigurations;
+
+public class RefreshTokenEntityTypeConfiguration : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.ToTable("RefreshTokens");
+        builder.HasKey(rt => rt.Id);
+
+        builder.Property(rt => rt.Id)
+            .HasConversion(id => id.Value, value => new RefreshTokenId(value));
+            
+        builder.Property(rt => rt.UserId)
+            .HasConversion(id => id.Value, value => new UserId(value));
+            
+        builder.HasIndex(rt => rt.Token).IsUnique();
+    }
+}
