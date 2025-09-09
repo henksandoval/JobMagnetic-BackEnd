47f2d602e1eb85881af373c75f27120a035c30e8
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-09-09 22:26:27 +0200

    feat: ✨ Add RegisterAsync and RefreshTokenAsync methods to IUserManagerAdapter for enhanced user management

--- sources/JobMagnet.Application/UseCases/Auth/Ports/IUserManagerAdapter.cs
+++ sources/JobMagnet.Application/UseCases/Auth/Ports/IUserManagerAdapter.cs
@@ -1,11 +1,15 @@
 using JobMagnet.Application.UseCases.Auth.DTO;
 using JobMagnet.Domain.Aggregates;
+using JobMagnet.Domain.Aggregates.Auth.Entities;
+using JobMagnet.Shared.Abstractions;
 
 namespace JobMagnet.Application.UseCases.Auth.Ports;
 
 public interface IUserManagerAdapter
 {
-    Task<UserToken> LoginAsync(UserModelCredentials userModelCredentials);
-    Task<UserToken> CreateAdminUserAsync(AdminUserOptions adminUserOptions,  CancellationToken cancellationToken);
     Task<UserToken> RegisterAsync(UserModelCredentials userModelCredentials);
+    Task<UserToken> LoginAsync(UserModelCredentials userModelCredentials);
+    
+    Task<UserToken> RefreshTokenAsync(RefreshToken  request);
+    Task<UserToken> CreateAdminUserAsync(AdminUserOptions adminUserOptions,  CancellationToken cancellationToken);
 }
