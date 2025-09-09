7d6a1cadd13cb0ca162d48e9653f1ab6fdeed2c9
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-09-09 22:23:53 +0200

    feat: ✨ Rename UserId class and update namespace for better organization

--- sources/JobMagnet.Domain/Aggregates/Auth/ValueObjects/UserId.cs
+++ sources/JobMagnet.Domain/Aggregates/Auth/ValueObjects/UserId.cs
@@ -1,5 +1,5 @@
 using JobMagnet.Domain.Shared.Base.Interfaces;
 
-namespace JobMagnet.Domain.Aggregates;
+namespace JobMagnet.Domain.Aggregates.Auth.ValueObjects;
 
 public readonly record struct UserId(Guid Value) : IStronglyTypedId<UserId>;
