799fcf5bd216d88a03f1f19b9b43383a2a5b8063
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-09-09 22:27:16 +0200

    feat: ✨ Add RefreshToken entity for managing user refresh tokens

---
+++ sources/JobMagnet.Domain/Aggregates/Auth/Entities/RefreshToken.cs
@@ -0,0 +1,31 @@
+using JobMagnet.Domain.Aggregates.Auth.ValueObjects;
+using JobMagnet.Domain.Aggregates.Profiles.ValueObjects;
+using JobMagnet.Domain.Shared.Base.Entities;
+using JobMagnet.Shared.Abstractions;
+
+namespace JobMagnet.Domain.Aggregates.Auth.Entities;
+
+public class RefreshToken : TrackableEntity<RefreshTokenId>
+{
+    public UserId UserId { get; private  set; }
+    public string Token { get; private  set; }
+    public DateTime Expires { get; private  set; }
+    public DateTime Created { get; private  set; }
+    public DateTime? Revoked { get; private  set; }
+    public  bool IsExpired => DateTime.UtcNow >= Expires;
+    public bool IsActive => Revoked == null && !IsExpired;
+    
+    private RefreshToken() { }
+    
+    internal static RefreshToken CreateInstance(IGuidGenerator guidGenerator, UserId userId, string token, TimeSpan validity)
+    {
+        return new RefreshToken
+        {
+            Id = new RefreshTokenId(guidGenerator.NewGuid()),
+            UserId = userId,
+            Token = token,
+            Created = DateTime.UtcNow,
+            Expires = DateTime.UtcNow.Add(validity)
+        };
+    }
+}
