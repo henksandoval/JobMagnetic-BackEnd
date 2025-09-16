335a0c5a9004ccf5e25a0624cf0b13882c4045ac
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-09-16 18:36:23 +0200

    feat: ✨ Refactor UserManagerAdapter to enhance user registration and token generation, add email existence check, and implement cancellation token support

--- sources/JobMagnet.Infrastructure/Services/Auth/UserManagerAdapter.cs
+++ sources/JobMagnet.Infrastructure/Services/Auth/UserManagerAdapter.cs
@@ -1,51 +1,64 @@
+using System.Diagnostics;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using JobMagnet.Application.UseCases.Auth.DTO;
 using JobMagnet.Application.UseCases.Auth.Ports;
 using JobMagnet.Application.UseCases.Auth.Ports.EmailDTO;
 using JobMagnet.Domain.Aggregates;
+using JobMagnet.Domain.Aggregates.Auth.Entities;
+using JobMagnet.Domain.Aggregates.Auth.ValueObjects;
+using JobMagnet.Domain.Ports.Repositories.Base;
 using JobMagnet.Infrastructure.Exceptions;
 using JobMagnet.Infrastructure.ExternalServices.Identity.Entities;
 using JobMagnet.Infrastructure.Services.EmailService.Interfaces;
+using JobMagnet.Shared.Abstractions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace JobMagnet.Infrastructure.Services.Auth;
 
-public class UserManagerAdapter(UserManager<ExternalServices.Identity.Entities.ApplicationIdentityUser> userManager, IConfiguration configuration, IEmailService emailService) : IUserManagerAdapter
+public class UserManagerAdapter(
+    UserManager<ApplicationIdentityUser> userManager,
+    IConfiguration configuration,
+    IEmailService emailService,
+    ICommandRepository<User> repository,
+    IUnitOfWork unitOfWork,
+    IGuidGenerator guidGenerator) : IUserManage
 {
-    private readonly UserManager<ExternalServices.Identity.Entities.ApplicationIdentityUser> _userManager = userManager;
-    private readonly IConfiguration _configuration = configuration;
-    private readonly IEmailService _emailService = emailService;
-    
-    public async Task<UserToken> RegisterAsync(UserModelCredentials  userModelCredentials)
+    private readonly IGuidGenerator _guidGenerator = guidGenerator;
+
+    public async Task<UserToken> RegisterAsync(UserModelCredentials  userModelCredentials, CancellationToken cancellationToken)
     {
-        
-        var userExists = await _userManager.FindByEmailAsync(userModelCredentials.Email);
-        if (userExists != null)
-        {
-            throw new EmailAlreadyTakenAdapterException($"The email'{userModelCredentials.Email}' already in use.");
-        }
-        
-        var user = new ApplicationIdentityUser
+        var appUser = new ApplicationIdentityUser
         {
             UserName = userModelCredentials.Email, 
-            Email = userModelCredentials.Email,
-            SecurityStamp = Guid.NewGuid().ToString()
+            Email = userModelCredentials.Email
         };
         
-        var result = await _userManager.CreateAsync(user, userModelCredentials.Password);
+        var identityResult  = await userManager.CreateAsync(appUser, userModelCredentials.Password);
         
-        if (result.Succeeded)
+        if (!identityResult.Succeeded)
         {
+            var errors = string.Join(", ", identityResult.Errors.Select(e => e.Description));
+            throw new InvalidOperationException($"Error de Identity: {errors}");
+        }
+        
+        try
+        {
+                
+            var domainUserId  = _guidGenerator.NewGuid();
+            var domainUser = User.AddUser(new UserId(domainUserId ), appUser.Email, null, appUser.Id);
+            await repository.CreateAsync(domainUser, cancellationToken);
+            await unitOfWork.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
             
-            var confirmationToken = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            var confirmationToken = await userManager.GenerateEmailConfirmationTokenAsync(appUser);
             var encodedToken = System.Net.WebUtility.UrlEncode(confirmationToken);
             
-            var confirmationUrlBase = _configuration["ClientApp:ConfirmationUrl"];
-            var confirmationLink = $"{confirmationUrlBase}?email={user.Email}&token={encodedToken}";
+            var confirmationUrlBase = configuration["ClientApp:ConfirmationUrl"];
+            var confirmationLink = $"{confirmationUrlBase}?email={appUser.Email}&token={encodedToken}";
             
             var mailCommand = new MailCommand
             {
@@ -59,26 +72,39 @@ public class UserManagerAdapter(UserManager<ExternalServices.Identity.Entities.A
                        """
             };
             
-            await  _emailService.SendEmailAsync(mailCommand);
-            return await BuildToken(user);
+            await  emailService.SendEmailAsync(mailCommand);
+            
+            return await GenerateAndSetTokensAsync(appUser);
+
         }
-        else
+        catch (Exception)
         {
-            var errors = string.Join(", ", result.Errors.Select(e => e.Description));
-            throw new InvalidOperationException($"No se pudo crear el usuario: {errors}");
+            await userManager.DeleteAsync(appUser);
+            throw;
         }
     }
 
-    public async Task<UserToken> LoginAsync(UserModelCredentials userModelCredentials)
+    public async Task<UserToken> LoginAsync(UserModelCredentials userModelCredentials, CancellationToken cancellationToken)
     {
-        var user = await _userManager.FindByEmailAsync(userModelCredentials.Email);
+        var identityUser = await userManager.FindByEmailAsync(userModelCredentials.Email);
+        if (identityUser == null || !await userManager.CheckPasswordAsync(identityUser, userModelCredentials.Password))
+            throw new InvalidCredentialsAdapterException("Incorrect email or password.");
 
-        if (user != null && await _userManager.CheckPasswordAsync(user, userModelCredentials.Password))
-        {
-            return await BuildToken(user);
-        }
-        
-        throw new InvalidCredentialsAdapterException("Incorrect email or password.");
+        // if (!await userManager.IsEmailConfirmedAsync(user))
+        //     throw new InvalidOperationException("Email not confirmed.");
+
+        return await GenerateAndSetTokensAsync(identityUser);
+    }
+
+    public async Task<bool> EmailExistAsync(string email)
+    {
+        var user = await userManager.FindByEmailAsync(email);
+        return user != null;
+    }
+
+    public  Task<UserToken> RefreshTokenAsync(RefreshToken request)
+    {
+            throw new NotImplementedException();
     }
 
     public async Task<UserToken> CreateAdminUserAsync (AdminUserOptions adminUserOptions, CancellationToken cancellationToken)
@@ -89,35 +115,34 @@ public class UserManagerAdapter(UserManager<ExternalServices.Identity.Entities.A
             Email = adminUserOptions.Email,
         };
         
-        var result = await _userManager.CreateAsync(applicationIdentityUser, adminUserOptions.Password);
+        var result = await userManager.CreateAsync(applicationIdentityUser, adminUserOptions.Password);
         if (result.Errors.Any(e => e.Code is "DuplicateUserName" or "DuplicateEmail"))
         {
             throw new EmailAlreadyTakenAdapterException($"The email'{adminUserOptions.Email}' already in use.");
         }
         
         var loginDto = new UserModelCredentials { Email = applicationIdentityUser.Email, Password = adminUserOptions.Password };
-        return await BuildToken(applicationIdentityUser);
+        return await GenerateAndSetTokensAsync(applicationIdentityUser);
     }
 
-    private async Task<UserToken> BuildToken(ApplicationIdentityUser user)
+    private async Task<UserToken> GenerateAndSetTokensAsync(ApplicationIdentityUser user)
     {
-        var userRoles = await _userManager.GetRolesAsync(user);
+        var userRoles = await userManager.GetRolesAsync(user);
         
-        var userClaims = await _userManager.GetClaimsAsync(user);
+        var userClaims = await userManager.GetClaimsAsync(user);
         
         var claims = new List<Claim>
         { 
             new Claim(JwtRegisteredClaimNames.Email, user.Email),
-            new Claim(ClaimTypes.Name, user.Email)
+            new Claim(ClaimTypes.Name, user.Email),
         };
         claims.AddRange(userRoles.Select(role => new Claim(ClaimTypes.Role, role)));
 
         claims.AddRange(userClaims);
         
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"]));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-        
-        var expiration = DateTime.UtcNow.AddHours(1);
+        var expiration = DateTime.UtcNow.AddMinutes(Convert.ToDouble(configuration["JWT:TokenValidityInMinutes"] ?? "15"));
         
         var securityToken = new JwtSecurityToken(
             issuer: null,
@@ -127,10 +152,32 @@ public class UserManagerAdapter(UserManager<ExternalServices.Identity.Entities.A
             signingCredentials: creds
         );
         
+        var accessToken = new JwtSecurityTokenHandler().WriteToken(securityToken);
+        
+        var refreshTokenResponse = GenerateRefreshTokenString();
+        var refreshTokenValidityInDays = Convert.ToInt32(configuration["JWT:RefreshTokenValidityInDays"] ?? "7");
+        var refreshTokenValidity = TimeSpan.FromDays(refreshTokenValidityInDays);
+
+        user.User.AddRefreshToken(
+            _guidGenerator, 
+            refreshTokenResponse, 
+            refreshTokenValidity
+        );
+
+       repository.Update(user.User);
+        
         return new UserToken()
         {
-            Token = new JwtSecurityTokenHandler().WriteToken(securityToken),
-            Expiration = expiration
+            Token = accessToken ,
+            Expiration = expiration,
+            RefreshToken = refreshTokenResponse
         };
+    }
+    private static string GenerateRefreshTokenString()
+    {
+        var randomNumber = new byte[64];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+        return Convert.ToBase64String(randomNumber);
     }
 }
