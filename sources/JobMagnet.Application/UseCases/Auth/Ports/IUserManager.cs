67147c84ac669e3dbc3700f2a9280f53b0bc490c
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-09-16 18:35:26 +0200

    feat: ✨ Rename IUserManagerAdapter to IUserManager and add cancellation token support to Register and Login methods

--- sources/JobMagnet.Application/UseCases/Auth/Ports/IUserManager.cs
+++ sources/JobMagnet.Application/UseCases/Auth/Ports/IUserManager.cs
@@ -5,11 +5,11 @@ using JobMagnet.Shared.Abstractions;
 
 namespace JobMagnet.Application.UseCases.Auth.Ports;
 
-public interface IUserManagerAdapter
+public interface IUserManage
 {
-    Task<UserToken> RegisterAsync(UserModelCredentials userModelCredentials);
-    Task<UserToken> LoginAsync(UserModelCredentials userModelCredentials);
-    
+    Task<UserToken> RegisterAsync(UserModelCredentials userModelCredentials, CancellationToken cancellationToken);
+    Task<UserToken> LoginAsync(UserModelCredentials userModelCredentials, CancellationToken cancellationToken);
+    Task<bool> EmailExistAsync(string email);    
     Task<UserToken> RefreshTokenAsync(RefreshToken  request);
     Task<UserToken> CreateAdminUserAsync(AdminUserOptions adminUserOptions,  CancellationToken cancellationToken);
 }
