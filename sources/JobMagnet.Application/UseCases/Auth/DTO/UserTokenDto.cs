bc424667c54ffba6fbe3ba962ac641d9fab3bd97
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-09-19 16:06:50 +0200

    feat: ✨ Rename UserToken to UserTokenDto for improved consistency in DTO usage

--- sources/JobMagnet.Application/UseCases/Auth/DTO/UserTokenDto.cs
+++ sources/JobMagnet.Application/UseCases/Auth/DTO/UserTokenDto.cs
@@ -1,6 +1,6 @@
 namespace JobMagnet.Application.UseCases.Auth.DTO;
 
-public class UserToken
+public class UserTokenDto
 {
     public string Token { get; set; } 
     public DateTime Expiration { get; set; }
