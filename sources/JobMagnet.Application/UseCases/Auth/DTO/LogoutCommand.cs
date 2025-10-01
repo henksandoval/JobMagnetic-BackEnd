9ae9ee0651bc1ac18237dafad16373eb2969500f
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-10-01 11:53:48 +0200

    feat: ✨ Add LogoutCommand class for handling user logout requests

---
+++ sources/JobMagnet.Application/UseCases/Auth/DTO/LogoutCommand.cs
@@ -0,0 +1,16 @@
+using CommunityToolkit.Diagnostics;
+
+namespace JobMagnet.Application.UseCases.Auth.DTO;
+public class LogoutCommand
+{
+    public string RefreshToken { get; set; }
+    public Guid UserId { get; set; }
+    
+    public LogoutCommand(string refreshToken, Guid userId)
+    {
+        Guard.IsNotNullOrWhiteSpace(refreshToken);
+
+        RefreshToken = refreshToken;
+        UserId = userId;
+    }
+}
