da057795e4271b4fa6c1809823e62450af970bae
Alexandra marval <Alexandra.marval@eurokaizen.com>
2025-10-01 11:52:54 +0200

    feat: ✨ Add ExceptionHandlerMiddleware to Program.cs for centralized error handling

--- sources/JobMagnet.Host/Program.cs
+++ sources/JobMagnet.Host/Program.cs
@@ -4,12 +4,12 @@ using JobMagnet.Application.Services;
 using JobMagnet.Application.UseCases.Auth.Ports.EmailDTO;
 using JobMagnet.Domain.Aggregates;
 using JobMagnet.Host.Extensions;
+using JobMagnet.Host.Middlewares;
 using JobMagnet.Host.Services;
 using JobMagnet.Infrastructure.Extensions;
-using JobMagnet.Infrastructure.Persistence.Context;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -36,11 +36,14 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         ValidateAudience = false,
         ValidateLifetime = true,
         ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["jwt:Key"] ?? string.Empty)),
+        // ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        // ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["jwt:Key"])),
         ClockSkew = TimeSpan.Zero
     });
 
 var app = builder.Build();
+app.UseMiddleware<ExceptionHandlerMiddleware>();
 
 if (builder.Configuration.GetValue<bool>("OpenApiSettings:UseUI"))
 {
