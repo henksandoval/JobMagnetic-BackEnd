6e52f7f334571bf76efec5b55f3a029a93687c73
Alexandra marval <Alexandra.marval@eurokaizen.com>
2026-01-20 19:25:49 +0100

    feat: ✨ Add constructor to ForgotPasswordCommand for email validation

--- sources/JobMagnet.Application/UseCases/Auth/DTO/ForgotPasswordCommand.cs
+++ sources/JobMagnet.Application/UseCases/Auth/DTO/ForgotPasswordCommand.cs
@@ -1,6 +1,14 @@
+using CommunityToolkit.Diagnostics;
+
 namespace JobMagnet.Application.UseCases.Auth.DTO;
 
 public class ForgotPasswordCommand
 {
     public string Email { get; set; }
+
+    public ForgotPasswordCommand(string email)
+    {
+        Guard.IsNotNullOrWhiteSpace(email);
+        Email = email;
+    }
 }
