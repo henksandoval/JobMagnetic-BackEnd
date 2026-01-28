b4087108f5f983c326e0fdbd612eee9303e8fbe8
Alexandra marval <Alexandra.marval@eurokaizen.com>
2026-01-28 14:28:10 +0100

    feat: ✨ Refactor AuthController to use Logout DTO and remove commented-out tests

--- tests/JobMagnet.Unit.Tests/AuthControllerShould.cs/AuthController.cs
+++ tests/JobMagnet.Unit.Tests/AuthControllerShould.cs/AuthController.cs
@@ -2,7 +2,7 @@ using System.Net;
 using AutoFixture;
 using AwesomeAssertions;
 using System.Security.Claims;
-using JobMagnet.Application.UseCases.Auth.DTO;
+using JobMagnet.Application.UseCases.Auth.DTO.Logout;
 using JobMagnet.Application.UseCases.Auth.Interface;
 using JobMagnet.Host.Controllers.V1;
 using Microsoft.AspNetCore.Http;
@@ -13,49 +13,14 @@ namespace JobMagnet.Unit.Tests.AuthControllerShould.cs;
 
 public class AuthControllerShould
 {
-    private readonly IFixture _fixture;
     private readonly Mock<IAuthUserHandler> _handlerMock; 
     private readonly AuthController _controller;
     
     public AuthControllerShould()
     {
-        _fixture = new Fixture();
         _handlerMock = new Mock<IAuthUserHandler>();
         _controller = new AuthController(_handlerMock.Object);
     }
-    
-    // [Fact]
-    // public async Task ReturnUnauthorized_WhenCredentialsAreInvalid()
-    // {
-    //     // --- Given ---
-    //     var loginDto = _fixture.Create<UserModelCredentials>();
-    //     _handlerMock.Setup(h => h.LoginAsync(loginDto)).ReturnsAsync((UserToken)null);
-    //
-    //     // --- When ---
-    //     var result = await _controller.LoginAsync(loginDto);
-    //
-    //     // --- Then ---
-    //     result.Should().BeOfType<UnauthorizedHttpResult>();
-    // }
-    //
-    // [Fact]
-    // public async Task ReturnToken_WhenCredentialsAreValid()
-    // {
-    //     // --- Given ---
-    //     var loginDTo = _fixture.Create<UserModelCredentials>();
-    //     var expectedToken = _fixture.Create<UserToken>();
-    //     _handlerMock.Setup(h => h.LoginAsync(loginDTo)).ReturnsAsync(expectedToken);
-    //     
-    //     // --- When ---
-    //     var result = await _controller.LoginAsync(loginDTo);
-    //     
-    //     // --- Then ---
-    //     var currentToken = result.Should()
-    //         .BeAssignableTo<Ok<UserToken>>()
-    //         .Subject.Value;
-    //     
-    //     currentToken.Should().BeEquivalentTo(expectedToken);
-    // }
 
     [Fact]
     public async Task LogoutAsync_WithValidTokenAndAuthenticatedUser_ReturnsNoContent()
