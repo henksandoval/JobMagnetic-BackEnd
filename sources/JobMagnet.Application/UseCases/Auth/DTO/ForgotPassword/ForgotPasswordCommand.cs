6761911daba9166a66fa155ccf70d57e4489c5fb
Alexandra marval <Alexandra.marval@eurokaizen.com>
2026-01-28 13:40:49 +0100

    feat: ✨ Refactor ForgotPasswordCommand namespace and make Email property immutable

--- sources/JobMagnet.Application/UseCases/Auth/DTO/ForgotPassword/ForgotPasswordCommand.cs
+++ sources/JobMagnet.Application/UseCases/Auth/DTO/ForgotPassword/ForgotPasswordCommand.cs
@@ -1,14 +1,15 @@
 using CommunityToolkit.Diagnostics;
 
-namespace JobMagnet.Application.UseCases.Auth.DTO;
+namespace JobMagnet.Application.UseCases.Auth.DTO.ForgotPassword;
 
 public class ForgotPasswordCommand
 {
-    public string Email { get; set; }
+    public string Email { get; }
 
     public ForgotPasswordCommand(string email)
     {
         Guard.IsNotNullOrWhiteSpace(email);
+        
         Email = email;
     }
 }
