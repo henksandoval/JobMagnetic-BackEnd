b23e988c56afbd791daadc444449583eecc93d18
Alexandra marval <Alexandra.marval@eurokaizen.com>
2026-04-16 22:24:08 +0200

    feat(auth): add DisplayName property to UserModelCredentialsDto for enhanced user details

--- sources/JobMagnet.Application/UseCases/Auth/DTO/UserModelCredentialsDto.cs
+++ sources/JobMagnet.Application/UseCases/Auth/DTO/UserModelCredentialsDto.cs
@@ -2,6 +2,7 @@ namespace JobMagnet.Application.UseCases.Auth.DTO;
 
 public class UserModelCredentialsDto
 {
+    public string DisplayName { get; set; } 
     public string Email { get; set; }
     public string Password { get; set; }
 }
