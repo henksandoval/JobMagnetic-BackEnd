2a8b83b523ba464c78d3571e7978e5b9c5dfe8be
Alexandra marval <Alexandra.marval@eurokaizen.com>
2026-04-23 17:57:12 +0200

    feat(auth): add ConfirmPasswordResetAsync method to IUserManager for password reset confirmation

--- sources/JobMagnet.Application/UseCases/Auth/Ports/IUserManager.cs
+++ sources/JobMagnet.Application/UseCases/Auth/Ports/IUserManager.cs
@@ -2,18 +2,22 @@ using JobMagnet.Application.UseCases.Auth.DTO;
 using JobMagnet.Application.UseCases.Auth.DTO.ForgotPassword;
 using JobMagnet.Application.UseCases.Auth.DTO.GoogleLogin;
 using JobMagnet.Application.UseCases.Auth.DTO.Logout;
+using JobMagnet.Application.UseCases.Auth.DTO.PasswordResetConfirm;
 using JobMagnet.Domain.Aggregates;
 
 namespace JobMagnet.Application.UseCases.Auth.Ports;
 
 public interface IUserManage
 {
-    Task<UserTokenDto> RegisterAsync(UserModelCredentialsDto userModelCredentialsDto, CancellationToken cancellationToken);
+    Task<UserTokenDto> RegisterAsync(UserModelCredentialsDto userModelCredentialsDto,
+        CancellationToken cancellationToken);
+
     Task<UserTokenDto> LoginAsync(UserModelCredentialsDto userModelCredentialsDto, CancellationToken cancellationToken);
     Task<GoogleTokenInfoDto> LoginGoogleAsync(GoogleLoginCommand loginCommand, CancellationToken cancellationToken);
-    Task<bool> EmailExistAsync(string email);    
-    Task<UserTokenDto> RefreshTokenAsync(RefreshTokenDto refreshTokenDto,  CancellationToken cancellationToken);
-    Task<UserTokenDto> CreateAdminUserAsync(AdminUserOptions adminUserOptions,  CancellationToken cancellationToken);
+    Task<bool> EmailExistAsync(string email);
+    Task<UserTokenDto> RefreshTokenAsync(RefreshTokenDto refreshTokenDto, CancellationToken cancellationToken);
+    Task<UserTokenDto> CreateAdminUserAsync(AdminUserOptions adminUserOptions, CancellationToken cancellationToken);
     Task<bool> LogoutAsync(LogoutCommand command, CancellationToken cancellationToken);
     Task GeneratePasswordResetTokenAsync(string email, CancellationToken cancellationToken);
+    Task ConfirmPasswordResetAsync(PasswordResetConfirmDto resetConfirmDto, CancellationToken cancellationToken);
 }
